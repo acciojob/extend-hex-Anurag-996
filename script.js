b4b971df2e354a8894b17c7fa0ece774fc9ce457
Anurag-996 <112651151+Anurag-996@users.noreply.github.com>
2023-12-08 15:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	// Remove # if present and convert to uppercase
+    shortHex = shortHex.replace("#", "").toUpperCase();
+
+    // Check if the shortHex has only one character per color
+    if (shortHex.length === 3) {
+        // Repeat each character to get the full hex code
+        shortHex = shortHex.split("").map(char => char.repeat(2)).join("");
+    }
+
+    // Add # prefix to the extended hex code
+    return "#" + shortHex;
 };
 
 // Do not change the code below.
